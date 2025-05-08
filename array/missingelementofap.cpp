e2ee7e7727f2eec5e1f946e40513a1ea3beec978
Ankit Gupta <114281117+ankitgupta143@users.noreply.github.com>
2025-05-08 11:07:29 +0530

    Add files via upload

---
+++ array/missingelementofap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    public:
+      int findMissing(vector<int> &arr) {
+          int n = arr.size();
+          int commonDiff = arr[1] - arr[0];
+          int ans = 0;
+          for(int i = 0; i < n; i++){
+              if(arr[i] + commonDiff != arr[i + 1]){
+                  ans  = arr[i] + commonDiff;
+                  break;
+              }
+          }
+          
+          return ans;
+      }
+  };
